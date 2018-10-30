0138b7d1ecdecc6b7c78307873421113f3b2fb54
hec <hecbuaa@gmail.com>
2018-10-30 15:51:40 -0700

    reminder
    just a reminder

--- coco.py
+++ coco.py
@@ -4,6 +4,7 @@ cap = dset.CocoCaptions(root='dir where images are',
                         annFile='json annotation file',
                         transform=transforms.ToTensor())
 
+
 print('Number of samples: ', len(cap))
 img, target = cap[3]  # load 4th sample
 
